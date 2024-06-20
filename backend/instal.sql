804997d1095c87be168f95c8b7a93fbbefe8c280
Denis <denis.tupalov62@gmail.com>
2024-06-20 20:56:31 +0300

    ver1

---
+++ backend/instal.sql
@@ -0,0 +1,9 @@
+CREATE TABLE  scheduler (
+    id INTEGER PRIMARY KEY AUTOINCREMENT id,
+    date VARCHAR,
+    title VARCHAR,
+    comment TEXT,
+    repeat VARCHAR(128)
+);
+
+CREATE INDEX  idx_date ON scheduler (date);
