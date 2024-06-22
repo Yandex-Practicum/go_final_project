eba6302198255b8fb124db3d75f9b24c95b20842
Denis <denis.tupalov62@gmail.com>
2024-06-22 20:32:41 +0300

    version 4

--- install.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE  scheduler (
-    id INTEGER PRIMARY KEY AUTOINCREMENT id,
-    date VARCHAR,
-    title VARCHAR,
-    comment TEXT,
-    repeat VARCHAR(128)
-);
-
-CREATE INDEX idx_date ON scheduler (date);
