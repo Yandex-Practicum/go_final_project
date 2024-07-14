b719964182ebbaaf1a86fbb51ba21a32f1d2710b
AlexJudin <spdante@mail.ru>
2024-07-15 00:07:55 +0300

    Added file server.

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	port := os.Getenv("TODO_PORT")
+	if len(port) == 0 {
+		port = "7540"
+	}
+
+	webDir := "./web"
+	r := chi.NewRouter()
+	r.Handle("/", http.FileServer(http.Dir(webDir)))
+
+	serverAddress := fmt.Sprintf("localhost:%s", port)
+	log.Println("Listening on " + serverAddress)
+	if err = http.ListenAndServe(serverAddress, http.FileServer(http.Dir(webDir))); err != nil {
+		log.Panicf("Start server error: %s", err.Error())
+	}
+}
