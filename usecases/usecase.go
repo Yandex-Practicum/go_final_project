d639683f0d5726eba07c3bacf80cdd2e71244699
AlexJudin <spdante@mail.ru>
2024-07-28 20:30:23 +0300

    Реализована обработка дней недели и месяца при расчете следующей даты задачи.

--- usecases/usecase.go
+++ usecases/usecase.go
@@ -2,6 +2,7 @@ package usecases
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -14,6 +15,9 @@ var _ Task = (*TaskUsecase)(nil)
 
 const (
 	year = 1
+
+	sundayEU  = 0
+	sundayRus = 7
 )
 
 type TaskUsecase struct {
@@ -25,6 +29,10 @@ func NewTaskUsecase(db repository.Task) *TaskUsecase {
 }
 
 func (t *TaskUsecase) GetNextDate(now time.Time, date string, repeat string) (string, error) {
+	if repeat == "" {
+		return "", fmt.Errorf("repeat is required")
+	}
+
 	dateTask, err := time.Parse("20060102", date)
 	if err != nil {
 		return "", err
@@ -45,8 +53,24 @@ func (t *TaskUsecase) GetNextDate(now time.Time, date string, repeat string) (st
 		dateTask = addDateTask(now, dateTask, 0, 0, days)
 	case "y":
 		dateTask = addDateTask(now, dateTask, year, 0, 0)
-	//case "w":
-	//case "m":
+	case "w":
+		if len(repeatString) < 2 {
+			return "", fmt.Errorf("repeat should be at least more two characters for weeks")
+		}
+
+		dateTask, err = getDateTaskByWeek(now, dateTask, repeatString[1])
+		if err != nil {
+			return "", err
+		}
+	case "m":
+		if len(repeatString) < 2 {
+			return "", fmt.Errorf("repeat should be at least two characters for month")
+		}
+
+		dateTask, err = getDateTaskByMonth(now, dateTask, repeatString)
+		if err != nil {
+			return "", err
+		}
 	default:
 		return "", fmt.Errorf("invalid character")
 	}
@@ -152,4 +176,113 @@ func addDateTask(now time.Time, dateTask time.Time, year int, month int, day int
 	}
 
 	return dateTask
+}
+
+func getDateTaskByWeek(now, dateTask time.Time, daysString string) (time.Time, error) {
+	days := strings.Split(daysString, ",")
+	daysOfWeek := regexp.MustCompile("[1-7]")
+
+	daysOfWeekMap := make(map[int]bool)
+	for _, day := range days {
+		numberOfDay, err := strconv.Atoi(day)
+		if err != nil {
+			return dateTask, err
+		}
+
+		if len(daysOfWeek.FindAllString(day, -1)) == 0 {
+			return dateTask, fmt.Errorf("invalid value %d day of the week", numberOfDay)
+		}
+
+		if numberOfDay == sundayRus {
+			numberOfDay = sundayEU
+		}
+		daysOfWeekMap[numberOfDay] = true
+	}
+
+	for {
+		if daysOfWeekMap[int(dateTask.Weekday())] {
+			if now.Before(dateTask) {
+				break
+			}
+		}
+		dateTask = dateTask.AddDate(0, 0, 1)
+	}
+
+	return dateTask, nil
+}
+
+func getDateTaskByMonth(now, dateTask time.Time, repeatRule []string) (time.Time, error) {
+	daysOfMounth := regexp.MustCompile("[1-31], -1, -2")
+	mounth := regexp.MustCompile("[1-12]")
+
+	daysString := repeatRule[1]
+	monthsString := repeatRule[2]
+
+	days := strings.Split(daysString, ",")
+	months := strings.Split(monthsString, ",")
+
+	daysMap := make(map[int]bool)
+	for _, dayString := range days {
+		day, err := strconv.Atoi(dayString)
+		if err != nil {
+			return dateTask, err
+		}
+		if len(daysOfMounth.FindAllString(dayString, -1)) == 0 {
+			return dateTask, fmt.Errorf("invalid value %d day of the month", day)
+		}
+		daysMap[day] = true
+	}
+
+	monthsMap := make(map[int]bool)
+	for _, monthString := range months {
+		if monthString != "" {
+			month, err := strconv.Atoi(monthString)
+			if err != nil {
+				return dateTask, err
+			}
+			if len(mounth.FindAllString(monthString, -1)) == 0 {
+				return dateTask, fmt.Errorf("invalid value %d month", month)
+			}
+			monthsMap[month] = true
+		}
+	}
+
+	found := false
+	for i := 0; i < 12*10; i++ {
+		month := int(dateTask.Month())
+		if len(monthsMap) > 0 && !monthsMap[month] {
+			dateTask = dateTask.AddDate(0, 1, 0)
+			dateTask = time.Date(dateTask.Year(), dateTask.Month(), 1, 0, 0, 0, 0, dateTask.Location())
+			continue
+		}
+
+		lastDayOfMonth := time.Date(dateTask.Year(), dateTask.Month()+1, 0, 0, 0, 0, 0, dateTask.Location()).Day()
+		for targetDay := range daysMap {
+			if targetDay > 0 {
+				if dateTask.Day() == targetDay && now.Before(dateTask) {
+					found = true
+					break
+				}
+			} else if targetDay < 0 {
+				if dateTask.Day() == lastDayOfMonth+targetDay+1 && now.Before(dateTask) {
+					found = true
+					break
+				}
+			}
+		}
+		if found {
+			break
+		}
+
+		dateTask = dateTask.AddDate(0, 0, 1)
+		if dateTask.Day() == 1 {
+			dateTask = time.Date(dateTask.Year(), dateTask.Month(), 1, 0, 0, 0, 0, dateTask.Location())
+		}
+	}
+
+	if !found {
+		return dateTask, nil
+	}
+
+	return dateTask, nil
 }
