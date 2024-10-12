eabbcc920fa72decb20dd512dd8667ad9fb6323a
Aleksandr <167681282+AlekxandrMoiseev@users.noreply.github.com>
2024-10-12 13:56:21 +0300

    new

---
+++ internal/model/model.go
@@ -0,0 +1,29 @@
+package model
+
+type Task struct {
+	ID      int64  `db:"id"`
+	Date    string `db:"date"`
+	Title   string `db:"title"`
+	Comment string `db:"comment"`
+	Repeat  string `db:"repeat"`
+}
+
+type AddTaskRequest struct {
+	ID      string `json:"id"`
+	Date    string `json:"date"`
+	Title   string `json:"title"`
+	Comment string `json:"comment,omitempty"`
+	Repeat  string `json:"repeat,omitempty"`
+}
+
+type TaskResponse struct {
+	ID      string `json:"id"`
+	Date    string `json:"date"`
+	Title   string `json:"title"`
+	Comment string `json:"comment"`
+	Repeat  string `json:"repeat"`
+}
+
+type TasksResponse struct {
+	Tasks []TaskResponse `json:"tasks"`
+}
