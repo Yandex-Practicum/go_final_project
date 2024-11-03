02f66aca7d02fcabdbc009a897ee7e6941ff2008
lezhnevay <lezhnevayy@yandex.ru>
2024-11-03 19:46:44 +0500

    first step

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 )
 
 func main() {
+
 	webDir := "./web"
+	http.Handle("/", http.FileServer(http.Dir(webDir)))
 	err := http.ListenAndServe(":7540", nil)
 	if err != nil {
 		panic(err)
 	}
 
-	http.Handle("/", http.FileServer(http.Dir(webDir)))
 }
