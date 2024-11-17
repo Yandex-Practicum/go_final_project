f0942360ff9e6325ac065741f6417c71a54f7d63
New-Beep <Lanco2701@Gmail.com>
2024-11-17 12:28:40 +0100

    latest

--- internal/repository/create_scheduler.go
+++ internal/repository/create_scheduler.go
@@ -2,14 +2,14 @@ package repository
 
 func (r *Repository) CreateScheduler() error {
 
-	query := `CREATE TABLE scheduler (
+	query := `CREATE TABLE IF NOT EXISTS scheduler (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		date CHAR(8) NOT NULL DEFAULT "",
 		title VARCHAR(256) NOT NULL DEFAULT "",
 		comment TEXT NOT NULL DEFAULT "",
 		repeat VARCHAR(128) NOT NULL DEFAULT "");
 	
-		CREATE INDEX tasks_rules ON scheduler (repeat);`
+		CREATE INDEX IF NOT EXISTS tasks_rules ON scheduler (repeat);`
 
 	_, err := r.db.Exec(query)
 	if err != nil {
