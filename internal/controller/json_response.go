b4a2edb4a40697c6bb736bdcbf49f008882018e1
New-Beep <Lanco2701@Gmail.com>
2024-11-18 08:56:33 +0100

    i hate git

--- internal/controller/json_response.go
+++ internal/controller/json_response.go
@@ -6,7 +6,7 @@ import (
 )
 
 func ResponseError(w http.ResponseWriter, s string) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusBadRequest)
 	json.NewEncoder(w).Encode(map[string]string{"error": s})
 }
