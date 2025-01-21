0f464825a13442c88f4791f5abbe95b8a50150ea
Seviat <aterley_s@mail.ru>
2025-01-21 11:02:49 +0300

    add editor and search Id

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ type TaskWithID struct {
 func NextDate(now time.Time, date string, repeat string) (string, error) {
 
 	if repeat == "" {
-		return "", errors.New("no set repeat rule")
+		return "", errors.New("не задано правило повтора")
 	}
 
 	// Парсим время
@@ -57,7 +57,7 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 	}
 
 	// Ежедневное выполнение задачи
-	if repeat == "d 1" {
+	if repeat == "d 1" && !dateParse.After(now) {
 		return now.Format(dateConst), nil
 	}
 
@@ -257,7 +257,8 @@ func getTaskHandler(w http.ResponseWriter, r *http.Request) {
 	if count > 0 {
 		// запрашиваем записи в порядке возрастания по дате
 		rows, err := db.Query(`SELECT id, date, title, comment, repeat
-			FROM scheduler ORDER BY date ASC LIMIT 50
+			FROM scheduler 
+			ORDER BY date ASC LIMIT 50
 		`)
 		if err != nil {
 			http.Error(w, `{"error":"Ошибка подсчета количества задач"}`, http.StatusInternalServerError)
@@ -288,6 +289,117 @@ func getTaskHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getTaskIdHandler(w http.ResponseWriter, r *http.Request) {
+	// подключение к бд
+	db, err := sql.Open("sqlite3", "scheduler.db")
+	if err != nil {
+		http.Error(w, `{"error":"Ошибка подключения к базе данных"}`, http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	// получение Id из запроса
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, `{"error":"Ошибка запроса Id к бд"}`, http.StatusBadRequest)
+		return
+	}
+
+	// получение задачи по Id из бд
+	var task TaskWithID
+	err = db.QueryRow(`SELECT id, date, title, comment, repeat	
+		FROM scheduler 
+		WHERE id = ?	`, id).Scan(&task.Id, &task.Date, &task.Title, &task.Comment, &task.Repeat)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, `{"error":"Задача не найдена"}`, http.StatusInternalServerError)
+		} else {
+			http.Error(w, `{"error":"Ошибка запроса к базе данных"}`, http.StatusInternalServerError)
+
+		}
+		return
+	}
+
+	// формируем ответ
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	err = json.NewEncoder(w).Encode(task)
+	if err != nil {
+		http.Error(w, `{"error":"Ошибка сериализации данных в JSON"}`, http.StatusInternalServerError)
+		return
+	}
+}
+
+func editTaskHandler(w http.ResponseWriter, r *http.Request) {
+
+	// подключение к бд
+	db, err := sql.Open("sqlite3", "scheduler.db")
+	if err != nil {
+		http.Error(w, `{"error":"Ошибка подключения к базе данных"}`, http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	var task TaskWithID
+
+	err = json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, `{"error":"Ошибка десериализации данных JSON"}`, http.StatusBadRequest)
+		return
+	}
+	// проверяем поля Id и Title
+	if task.Id == "" {
+		http.Error(w, `{"error":"Id не задан"}`, http.StatusBadRequest)
+		return
+	}
+
+	if task.Title == "" {
+		http.Error(w, `{"error":"Task не задано"}`, http.StatusBadRequest)
+		return
+	}
+
+	// обработка поля Date
+	now := time.Now()
+	if task.Date == "" {
+		task.Date = now.Format(dateConst)
+	} else {
+		parsedDate, err := time.Parse(dateConst, task.Date)
+		if err != nil {
+			http.Error(w, `{"error":"Неверный формат даты"}`, http.StatusBadRequest)
+			return
+		}
+		if parsedDate.Before(now) && task.Repeat != "" {
+			nextDate, err := NextDate(now, task.Date, task.Repeat)
+			if err != nil {
+				http.Error(w, `{"error":"Неверный формат правила"}`, http.StatusInternalServerError)
+				return
+			}
+			task.Date = nextDate
+		}
+	}
+
+	res, err := db.Exec(`UPDATE scheduler 
+		SET date = ?, title = ?, comment = ?, repeat = ? WHERE id = ?
+		`, task.Date, task.Title, task.Comment, task.Repeat, task.Id)
+
+	if err != nil {
+		http.Error(w, `{"error":"Ошибка редактирования задачи"}`, http.StatusInternalServerError)
+		return
+	}
+
+	// проверяем редактирование задачи
+	affected, err := res.RowsAffected()
+	if err != nil || affected == 0 {
+		http.Error(w, `{"error":"Задача не найдена"}`, http.StatusInternalServerError)
+		return
+	}
+
+	// возвращаем пустой JSON-ответ, если все хорошо
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Write([]byte("{}"))
+}
+
 func main() {
 
 	db, err := initDB()
@@ -300,8 +412,10 @@ func main() {
 
 	r.Handle("/*", http.FileServer(http.Dir(webDir)))
 	r.Get("/api/nextdate", nextDateHandler)
+	r.Get("/api/task", getTaskIdHandler)
 	r.Post("/api/task", addTaskHandler)
-	r.Get("/api/task", getTaskHandler)
+	r.Put("/api/task", editTaskHandler)
+	r.Get("/api/tasks", getTaskHandler)
 
 	err = http.ListenAndServe(":"+port, r)
 	if err != nil {
