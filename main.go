676f9e95cac800de3e24c9461525ad8d3c562957
Ekaterina Deribina <dekatei@yandex.ru>
2025-03-17 01:26:02 +0300

    easy repeat function

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"main.go/base"
 )
@@ -23,17 +25,46 @@ func mainHandle(res http.ResponseWriter, req *http.Request) {
 
 const webDir = "./web"
 
+func nextDateHandle(w http.ResponseWriter, req *http.Request) {
+	nowStr := req.URL.Query().Get("now")
+	if nowStr == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("now missing"))
+		err := errors.New("пропущено время")
+		fmt.Println(err)
+	}
+
+	now, err := time.Parse("20060102", nowStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("wrong count value"))
+		fmt.Println(err)
+	}
+
+	date := req.URL.Query().Get("date")
+	repeat := req.URL.Query().Get("repeat")
+	nextDate, err := base.NextDate(now, date, repeat)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(nextDate))
+
+}
+
 func main() {
 	fmt.Println("Запускаем сервер")
 	//http.HandleFunc(`/`, mainHandle)
 	// Устанавливаем обработчик для корневого URL
 	http.Handle("/", http.FileServer(http.Dir(webDir)))
+	http.HandleFunc(`/api/nextdate`, nextDateHandle)
 	envPort := os.Getenv("TODO_PORT")
 	if envPort == "" {
 		envPort = "7540"
 	}
 	envDBFILE := os.Getenv("TODO_DBFILE")
-	base.CreateTable(envDBFILE)
+	base.CreateDB(envDBFILE)
 	err := http.ListenAndServe(":"+envPort, nil)
 	if err != nil {
 		panic(err)
